01dee54c2b90e925c7e88f8702759c5dbece3492
Dayoung <dbsek21@naver.com>
2023-05-02 22:05:02 +0900

    Feat: csrf 활성화

--- src/main/java/com/web/demo/config/auth/SecurityConfig.java
+++ src/main/java/com/web/demo/config/auth/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()// csrf토큰이 없이 요청하면 해당 요청을 막기 때문에 비활성화
+                //.csrf().disable()// csrf토큰이 없이 요청하면 해당 요청을 막기 때문에 비활성화
                 .authorizeRequests()//request에 따라 접근을 제한
                 .antMatchers("/", "/auth/**", "/posts/detail/**", "/posts/search/**")// 특정 경로에 대해서
                 .permitAll()// 접근이 가능하다.
